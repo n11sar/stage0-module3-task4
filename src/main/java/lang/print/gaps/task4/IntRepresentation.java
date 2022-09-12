238b6e9fb44dd3023566c9ba44900a29c5f66960
georgy <ganus1111@gmail.com>
2022-09-12 22:52:45 +0300

    solve task

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        System.out.println(99);
+        System.out.println(104);
+        System.out.println(97);
+        System.out.println(114);
+
     }
 }
